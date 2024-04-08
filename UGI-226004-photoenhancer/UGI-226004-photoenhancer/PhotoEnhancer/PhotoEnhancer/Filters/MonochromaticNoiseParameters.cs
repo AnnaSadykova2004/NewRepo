36b6825542aeb63bbc84acffdf801c0c9a7ff4a4
Anna.Sadykova@urfu.me <Anna.Sadykova@urfu.me>
2024-04-08 23:07:06 +0500

    добавлен новый фильтр "Монохромный шум"

---
+++ UGI-226004-photoenhancer/UGI-226004-photoenhancer/PhotoEnhancer/PhotoEnhancer/Filters/MonochromaticNoiseParameters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer.Filters
+{
+    public class MonochromaticNoiseParameters : IParameters
+    {
+        public double Coefficient { get; set; }
+
+        public ParameterInfo[] GetDescription()
+        {
+            return new[]
+            {
+                new ParameterInfo()
+                {
+                    Name = "Интенсивность шума",
+                    MinValue = 0,
+                    MaxValue =1,
+                    DefaultValue = 0,
+                    Increment = 0.01
+                }
+            };
+        }
+
+        public void SetValues(double[] values)
+        {
+            Coefficient = values[0];
+        }
+
+    }
+}
